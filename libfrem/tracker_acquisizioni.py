82927a228d0fce96d9aaa6a50d04fee16e801448
giacomounivr <giacomo.marchioro@univr.it>
2021-03-18 15:34:43 +0100

    Update

---
+++ libfrem/tracker_acquisizioni.py
@@ -0,0 +1,41 @@
+import sqlalchemy as db
+from datetime import datetime
+import os
+from pathlib import Path
+import glob
+
+#dirlist = [filename for filename in os.listdir(os.getcwd()) if os.path.isdir(filename)]
+
+with os.scandir(os.getcwd()) as mydir:
+    dirs = [i.name for i in mydir if i.is_dir()]
+
+
+
+
+
+# campi fissi
+stanza = 205
+# Create the database or access it.
+engine = db.create_engine('sqlite:///acquisizioni_server.sqlite')
+connection = engine.connect()
+metadata = db.MetaData()
+for i in dirs:
+    jp2acqu = len(glob.glob1(i,"*.jp2"))
+    tiffacqu = len(glob.glob1(i,"*.tiff"))
+    print(jp2acqu)
+    emp = db.Table('movimenti_stanza_%s'%stanza, metadata,
+              db.Column('Time_stamp', db.DateTime()),
+              db.Column('ID_bene', db.Integer()),
+              db.Column('manifest', db.String())    ,
+              db.Column('peso_cg', db.Integer()),
+              # codice bilancia?
+              # utente loggato?
+              # motivazione spostamento?
+              )
+    metadata.create_all(engine) #  Creates the table
+
+     query = db.insert(emp).values(Time_stamp=now,
+                                   ID_bene=int(object_ID),
+                                   destinazione=int(destinazione_ID),
+                                   peso_cg=int(peso*100))
+    ResultProxy = connection.execute(query)
